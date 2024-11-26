035204ac635027401d744b3862a28d973a852088
Yuli <yukengli@asu.edu>
2024-11-25 22:23:15 -0700

    Add files via upload

---
+++ Denoising.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import tensorflow as tf
+import keras 
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPool2D,UpSampling2D,Dropout
+
+from keras.datasets import mnist
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+# to get the shape of the data 
+
+'''
+train_filter = np.isin(y_train, [1,5,9])
+x_train = x_train[train_filter]
+y_train = y_train[train_filter]
+
+test_filter = np.isin(y_test, [1,5,9])
+x_test = x_test[test_filter]
+y_test = y_test[test_filter]
+'''
+
+val_images = x_test[:9000]
+test_images = x_test[9000:]
+
+val_images = val_images.astype('float32') / 255.0
+val_images = np.reshape(val_images,(val_images.shape[0],28,28,1))
+
+test_images = test_images.astype('float32') / 255.0
+test_images = np.reshape(test_images,(test_images.shape[0],28,28,1))
+
+train_images = x_train.astype("float32") / 255.0
+train_images = np.reshape(train_images, (train_images.shape[0],28,28,1))
+
+factor = 0.39
+train_noisy_images = train_images + factor * np.random.normal(loc = 0.0,scale = 1.0,size = train_images.shape)
+val_noisy_images = val_images + factor * np.random.normal(loc = 0.0,scale = 1.0,size = val_images.shape)
+test_noisy_images = test_images + factor * np.random.normal(loc = 0.0,scale = 1.0,size = test_images.shape)
+
+# here maximum pixel value for our images may exceed 1 so we have to clip the images
+train_noisy_images = np.clip(train_noisy_images,0.,1.)
+val_noisy_images = np.clip(val_noisy_images,0.,1.)
+test_noisy_images = np.clip(test_noisy_images,0.,1.)
+
+model = Sequential()
+# encoder network
+model.add(Conv2D(filters = 64, kernel_size = (3,3), activation = 'relu', padding = 'same', input_shape = (28,28,1)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(filters = 128, kernel_size = (2,2),strides = (2,2), activation = 'relu', padding = 'same'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(filters = 256, kernel_size = (2,2),strides = (2,2), activation = 'relu', padding = 'same'))
+
+model.add(Conv2D(filters = 256, kernel_size = (2,2), activation = 'relu', padding = 'same'))
+model.add(tf.keras.layers.Conv2DTranspose(filters = 128, kernel_size = (2,2), strides = (2,2),activation = 'relu', padding = 'same'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2DTranspose(filters = 64, kernel_size = (2,2), strides = (2,2),activation = 'relu', padding = 'same'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(filters = 1, kernel_size = (2,2), activation = 'relu', padding = 'same'))
+
+# to get the summary of the model
+model.summary()
+
+OPTIMIZER =  tf.keras.optimizers.Adam(learning_rate = 0.001)
+LOSS = 'mean_squared_error'
+model.compile(optimizer =OPTIMIZER, loss = LOSS, metrics = ['accuracy'])
+
+EPOCHS = 5
+BATCH_SIZE = 16
+VALIDATION = (val_noisy_images, val_images)
+history = model.fit(train_noisy_images, train_images,batch_size = BATCH_SIZE,epochs = EPOCHS, validation_data = VALIDATION)
+
+plt.subplot(2,1,1)
+plt.plot( history.history['loss'], label = 'loss')
+plt.plot( history.history['val_loss'], label = 'val_loss')
+plt.legend(loc = 'best')
+plt.subplot(2,1,2)
+plt.plot( history.history['accuracy'], label = 'accuracy')
+plt.plot( history.history['val_accuracy'], label = 'val_accuracy')
+plt.legend(loc = 'best')
+plt.show()
+
+plt.figure(figsize = (18,18))
+for i in range(10,19):
+    plt.subplot(9,9,i)
+    if(i == 14):
+        plt.title('Real Images', fontsize = 25, color = 'Green') 
+    plt.imshow(test_images[i].reshape(1,28,28)[0], cmap = plt.cm.binary)
+plt.show()
+
+
+plt.figure(figsize = (18,18))
+for i in range(10,19):
+    if(i == 15):
+        plt.title('Noised Images', fontsize = 25, color = 'red') 
+    plt.subplot(9,9,i)
+    plt.imshow(test_noisy_images[i].reshape(1,28,28)[0], cmap = plt.cm.binary)
+plt.show()
+
+plt.figure(figsize = (18,18))
+for i in range(10,19):  
+    if(i == 15):
+        plt.title('Denoised Images', fontsize = 25, color = 'Blue') 
+    plt.subplot(9,9,i)
+    plt.imshow(model.predict(test_noisy_images[i].reshape(1,28,28,1)).reshape(1,28,28)[0], cmap = plt.cm.binary) 
+plt.show()
